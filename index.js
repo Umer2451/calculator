ce2702ecdcee8e32ae386976f807532b33d0213f
Syed Umer Naeem <Umer.Naeem@tpsonline.com>
2022-10-27 23:25:37 +0500

    new commit

--- index.js
+++ index.js
@@ -4,9 +4,37 @@ const app = express() //app using express function
 const port = 3000 //what port to use
 
 const path = require('path')
+const res = require('express/lib/response')
+const prompt = require("prompt");
 
 app.get('/users', (req, res) => { //request = req, and res = response
   res.send('Hello Umer! Im WORKING fine') //send request.
+})
+
+app.get('/evenorodd', (req,res) => {
+  function evenorodd() {
+  
+    prompt.start();
+    
+    prompt.get(["num"], 
+    function (err, res) {
+      if (err) {
+        console.log(err);
+      } else {
+
+      if(res.num % 2 == 0) {
+        console.log("The number is even.");
+        }
+
+// if the number is odd :##
+      else {
+        console.log("The number is odd.");
+        } 
+        }
+    });
+  }
+  evenorodd();
+
 })
 
 app.listen(port, () => {
