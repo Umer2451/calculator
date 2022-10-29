273bc4be99e43fea80e4cdc9682924f7db834e98
nriaz2560@gmail.com <nriaz2560@gmail.com>
2022-10-29 08:35:22 -0700

    Nimra-Riaz

--- index.js
+++ index.js
@@ -3,10 +3,22 @@ const { deflate } = require('zlib')
 const app = express() //app using express function
 const port = 3000 //what port to use
 
+const prompt = require('prompt-sync')();
+
+
 const path = require('path')
 
 app.get('/users', (req, res) => { //request = req, and res = response
   res.send('Hello Umer! Im WORKING fine') //send request.
+})
+
+// Addition Operation API
+app.get('/add', (req, res) => {
+  const number1 = parseInt(prompt('Enter first number?'));
+  const number2 =parseInt(prompt('Enter second number?'));
+  sum=number1+number2;
+  console.log(sum)
+res.send("Answer="+sum+"")
 })
 
 app.listen(port, () => {
